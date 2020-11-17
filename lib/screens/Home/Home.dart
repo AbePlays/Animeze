5ac3d35d33417ddf8f950e9f0ddf33369d781558
Abe <abhi.rawat456@gmail.com>
2020-11-17 10:26:56 +0530

    Added a drawer

--- lib/screens/Home/Home.dart
+++ lib/screens/Home/Home.dart
@@ -10,6 +10,17 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.wb_sunny),
+              title: Text("Dark Mode"),
+              trailing: Switch(value: false, onChanged: null),
+            )
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
