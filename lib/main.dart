bf505dfd45691e085a661d839805bb0c4f6240f3
Abe <abhi.rawat456@gmail.com>
2020-11-19 16:37:00 +0530

    Disabled debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animeze',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
       home: Home(),
     );
