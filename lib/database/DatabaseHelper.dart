fc5d1a2e7998e6b30e71e181513d5767f88d195e
Abe <abhi.rawat456@gmail.com>
2020-11-19 17:28:15 +0530

    Created SQL table

---
+++ lib/database/DatabaseHelper.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "MyDatabase.db";
+  static final _databaseVersion = 1;
+  static final table = 'anime_table';
+  static final id = 'id';
+  static final imageUrl = 'imageUrl';
+  static final title = 'title';
+  static final score = 'score';
+  static final dateReleased = 'dateReleased';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await initDatabase();
+    return _database;
+  }
+
+  initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: onCreate);
+  }
+
+  Future onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $id INTEGER PRIMARY KEY,
+            $imageUrl TEXT NOT NULL,
+            $title TEXT NOT NULL
+            $score REAL NOT NULL
+            $dateReleased TEXT NOT NULL
+          )
+          ''');
+  }
+}
---
+++ lib/model/AnimeModel.dart
@@ -0,0 +1,9 @@
+class Anime {
+  final int id;
+  final String imageUrl;
+  final String title;
+  final int score;
+  final String dateRelease;
+
+  Anime({this.id, this.imageUrl, this.title, this.score, this.dateRelease});
+}
